e9598490102fda9154781542336d06a99a035ed5
niklasmodig132 <niklas@kumpan.se>
2023-02-16 22:23:27 +0100

    tutorial 1-6

---
+++ react-tutorial/src/navbar.js
@@ -0,0 +1,17 @@
+const Navbar = () => {
+    return ( 
+        <nav className="navbar">
+<h1>Niklas Modig</h1>
+<div className="links">
+    <a href="/">Home</a>
+    <a href="/create" style={{
+        color: "white", 
+        backgroundColor: '#f1356d',
+        borderRadius: '8px'
+    }}>New blog</a> 
+</div>
+        </nav>
+         );
+}
+ 
+export default Navbar;
